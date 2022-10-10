c9db6d0ba5fa2c38737acc1a1a48c33135b5ae65
LucianT93 <lucianlukas93@gmail.com>
2022-10-10 19:53:39 +0300

    Brake the login, logout and rating forms from photography views into multiple functions Updated urls and template from photography views to match the new functions

--- photography/urls.py
+++ photography/urls.py
@@ -4,5 +4,8 @@ from photography import views
 
 urlpatterns = [
     path('', views.PhotoPostHome.as_view(), name='home'),
-    path('logout/', views.logoutUser, name='logout'),
+    path('logout/', views.user_logout, name='logout'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.user_register, name='register'),
+    path('rating_form/', views.rating_form, name='rating_form')
 ]
--- photography/views.py
+++ photography/views.py
@@ -12,112 +12,65 @@ from photography.forms import RatingForm, LoginForm, SignUpForm
 from photography.models import Photography, Rating
 
 
-# Create your views here.
-
-
-# def photo_posts(request):
-#     all_photos = Photography.objects.all()
-#     form_rating = RatingForm()
-#     form_login = LoginForm()
-#     form_signup = SignUpForm()
-#
-#     if request.method == 'POST':
-#         if request.POST.get('username') and request.POST.get('password'):
-#             username = request.POST['username']
-#             password = request.POST['password']
-#
-#             try:
-#                 user = User.objects.get(username=username)
-#             except:
-#                 messages.error(request, 'Username does not exists')
-#
-#             user = authenticate(request, username=username, password=password)
-#
-#             if user:
-#                 login(request, user)
-#                 messages.success(request, 'Logged it successfully ')
-#                 return redirect('home')
-#             else:
-#                 messages.error(request, 'Username or password incorrect!')
-#         elif request.POST.get('value') and request.POST.get('body'):
-#             form_rating = RatingForm(request.POST)
-#             photo_obj = Photography.objects.get(id=request.POST.get('photo_id'))
-#             if form_rating.is_valid():
-#                 rating = form_rating.save(commit=False)
-#                 rating.photography = photo_obj
-#                 rating.owner = request.user.profile
-#                 rating.save()
-#         else:
-#             print(request.POST)
-#             form_signup = SignUpForm(request.POST)
-#             if form_signup.is_valid():
-#                 user = form_signup.save(commit=False)
-#                 user.username = user.username.lower()
-#                 user.save()
-#                 messages.success(request, 'User account was created! ')
-#
-#     return render(request, 'home/home.html', {
-#         'all_photos': all_photos,
-#         'form_rating': form_rating,
-#         'form_login': form_login,
-#         'form_signup': form_signup,
-#     })
-
 class PhotoPostHome(TemplateView):
     template_name = 'home/home.html'
 
-    def get(self, request, *args, **kwargs):
+    def get_context_data(self, **kwargs):
+        data = super(PhotoPostHome, self).get_context_data(**kwargs)
         all_photos = Photography.objects.all().order_by('-created')
         form_rating = RatingForm()
         form_login = LoginForm()
         form_signup = SignUpForm()
 
-        return self.render_to_response({
-            'form_rating': form_rating,
-            'form_login': form_login,
-            'form_signup': form_signup,
-            'all_photos': all_photos
-        })
+        data['form_rating'] = form_rating
+        data['form_login'] = form_login
+        data['form_signup'] = form_signup
+        data['all_photos'] = all_photos
 
-    def post(self, request, *args, **kwargs):
-        if request.POST.get('form_login') == 'f_login':
-            username = request.POST['username']
-            password = request.POST['password']
-
-            try:
-                user = User.objects.get(username=username)
-            except:
-                messages.error(request, 'Username does not exists')
-
-            user = authenticate(request, username=username, password=password)
-
-            if user:
-                login(request, user)
-                messages.success(request, 'Logged it successfully ')
-                return redirect('home')
-            else:
-                messages.error(request, 'Username or password incorrect!')
-                return redirect('home')
-
-        if request.POST.get('form_signup') == 'f_signup':
-            form_signup = SignUpForm(request.POST)
-            if form_signup.is_valid():
-                user = form_signup.save(commit=False)
-                user.username = user.username.lower()
-                user.save()
-                messages.success(request, 'User account was created! ')
-            return redirect('home')
-        if request.POST.get('form_rating') == 'f_rating':
-            form_rating = RatingForm(request.POST)
-            photo_obj = Photography.objects.get(id=request.POST.get('photo_id'))
-            if form_rating.is_valid():
-                rating = form_rating.save(commit=False)
-                rating.photography = photo_obj
-                rating.owner = request.user.profile
-                rating.save()
-            return redirect('home')
+        return data
 
 
-def logoutUser(request):
+def user_logout(request):
     logout(request)
+    return redirect('home')
+
+
+def user_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    try:
+        user = User.objects.get(username=username)
+    except:
+        messages.error(request, 'Username does not exists')
+
+    user = authenticate(request, username=username, password=password)
+
+    if user:
+        login(request, user)
+        messages.success(request, 'Logged it successfully ')
+        return redirect('home')
+    else:
+        messages.error(request, 'Username or password incorrect!')
+        return redirect('home')
+
+
+def user_register(request):
+    form_signup = SignUpForm(request.POST)
+    if form_signup.is_valid() and not form_signup.errors:
+        user = form_signup.save(commit=False)
+        user.username = user.username.lower()
+        user.save()
+        messages.success(request, 'User account was created! ')
+    return redirect('home')
+
+
+def rating_form(request):
+    form_rating = RatingForm(request.POST)
+    photo_obj = Photography.objects.get(id=request.POST.get('photo_id'))
+    if form_rating.is_valid():
+        rating = form_rating.save(commit=False)
+        rating.photography = photo_obj
+        rating.owner = request.user.profile
+        rating.save()
     return redirect('home')
